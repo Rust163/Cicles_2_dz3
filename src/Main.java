1b9eaab6652c2fcb1cbbca3406c2939bbddcd925
Рустам <rustam19871203@gmail.com>
2022-10-19 21:18:45 +0400

    Циклы часть 2. Домашка 3.

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+            //Задание 1
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 1896; i <= 2054; i += 79){
+            System.out.println(i);
+        }
+
+        System.out.println();
+
+            //Задача 2
+            int l = 2;
+            for(int m = 1; m <= 10; m++) {
+            System.out.println(l + "*" + m + "=" + l * m);
+        }
+
+    }
+}
